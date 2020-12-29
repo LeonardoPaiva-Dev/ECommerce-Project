637bcaadaf7bcdbc9a47d8cb26c51bc566553337
Leonardo Paiva <leonardopaivagamedev@gmail.com>
2020-12-29 18:26:17 -0300

    Cart page data

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -31,16 +31,35 @@ class Pedido(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, blank=True, null=True)
     data_pedido = models.DateTimeField(auto_now_add=True)
     completo = models.BooleanField(default=False, null=True, blank=False)
+    objects = models.Manager()
     id_transacao = models.CharField(max_length=200, null=True)
+    
 
     def __str__(self):
         return str(self.id)
-     
+
+    @property
+    def get_cart_total(self):
+        orderitems = self.itempedido_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.itempedido_set.all()
+        total = sum([item.quantidade for item in orderitems])
+        return total
+
 class ItemPedido(models.Model):
     produto = models.ForeignKey(Produto, on_delete=models.SET_NULL, blank=True, null=True)
     pedido = models.ForeignKey(Pedido, on_delete=models.SET_NULL, blank=True, null=True)
     quantidade = models.IntegerField(default=0, null=True, blank=True)
     data_pedido = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.produto.preco * self.quantidade
+        return total
 
 class LocalEntrega(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, blank=True, null=True)
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -9,7 +9,16 @@ def store(request):
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.cliente
+        order, created = Pedido.objects.get_or_create(cliente=customer, completo=False)
+        items = order.itempedido_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {'items':items, 'order':order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
