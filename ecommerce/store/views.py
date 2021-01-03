8ccd58727fd5d1f8443ad5ffe1398857ab4d7064
Leonardo Paiva <leonardopaivagamedev@gmail.com>
2021-01-03 00:48:35 -0300

    Checkout page data

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -22,5 +22,13 @@ def cart(request):
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.cliente
+        order, created = Pedido.objects.get_or_create(cliente=customer, completo=False)
+        items = order.itempedido_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
