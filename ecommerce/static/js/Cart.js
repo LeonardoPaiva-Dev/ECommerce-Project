1537d89e122fe941a822793e48522a5a0b7f4541
Leonardo Paiva <leonardopaivagamedev@gmail.com>
2021-01-04 00:29:22 -0300

    add and update cart event handlers

--- ecommerce/static/js/Cart.js
+++ ecommerce/static/js/Cart.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+var updateButtons = document.getElementsByClassName('update-cart')
+
+for(i = 0; i < updateButtons.length; i++){
+    updateButtons[i].addEventListener('click', function(){
+        var productId = this.dataset.product  
+        var action = this.dataset.action
+        console.log('productID: ', productId, 'Action: ', action)
+
+        console.log('USER: ', user)
+        if(user == 'AnonymousUser'){
+            console.log('Usuario não autenticado')
+        }
+        else{
+            console.log('usuario autenticado, enviando dados...')
+        }
+
+
+    })
+}
