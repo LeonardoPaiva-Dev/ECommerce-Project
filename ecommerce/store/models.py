b823f016f8dfb5f8c8bad0432dd567da5a213fe1
Leonardo Paiva <leonardopaivagamedev@gmail.com>
2021-01-14 19:46:25 -0300

    shipping information

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -39,6 +39,15 @@ class Pedido(models.Model):
         return str(self.id)
 
     @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.itempedido_set.all()
+        for i in orderitems:
+            if i.produto.digital == False:
+                shipping = True
+        return shipping
+
+    @property
     def get_cart_total(self):
         orderitems = self.itempedido_set.all()
         total = sum([item.get_total for item in orderitems])
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -15,7 +15,7 @@ def store(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
         cartItems = order['get_cart_items']
 
     products = Produto.objects.all()
@@ -30,7 +30,7 @@ def cart(request):
         items = order.itempedido_set.all()
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
 
     context = {'items':items, 'order':order}
     return render(request, 'store/cart.html', context)
@@ -42,7 +42,7 @@ def checkout(request):
         items = order.itempedido_set.all()
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
 
     context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
